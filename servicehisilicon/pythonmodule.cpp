557d09c14b97fff8c237ce0516423b4fa21e5712
Twol <tonywhalley@t-online.de>
2023-11-21 12:06:07 +0100

    [servichisilicon][pythonmodule] - python3 only

--- servicehisilicon/pythonmodule.cpp
+++ servicehisilicon/pythonmodule.cpp
@@ -2,31 +2,22 @@
 
 static PyMethodDef servicehisiliconMethods[] =
 {
-	 {NULL,NULL,0,NULL}
+	{NULL,NULL,0,NULL}
 };
 
-#if PY_MAJOR_VERSION >= 3
-	static struct PyModuleDef moduledef = {
-		PyModuleDef_HEAD_INIT,
-		"servicehisilicon",					/* m_name */
-		"Module for servicehisilicon",		/* m_doc */
-		-1,									/* m_size */
-		servicehisiliconMethods,			/* m_methods */
-		NULL,								/* m_reload */
-		NULL,								/* m_traverse */
-		NULL,								/* m_clear */
-		NULL,								/* m_free */
-	};
+static struct PyModuleDef moduledef = {
+	PyModuleDef_HEAD_INIT,
+	"servicehisilicon",		/* m_name */
+	"Module for servicehisilicon",	/* m_doc */
+	-1,				/* m_size */
+	servicehisiliconMethods,	/* m_methods */
+	NULL,				/* m_reload */
+	NULL,				/* m_traverse */
+	NULL,				/* m_clear */
+	NULL,				/* m_free */
+};
 
 PyMODINIT_FUNC PyInit_servicehisilicon(void)
 {
-    return PyModule_Create(&moduledef);
+	return PyModule_Create(&moduledef);
 }
-#else
-
-PyMODINIT_FUNC
-initservicehisilicon(void)
-{
-	Py_InitModule("servicehisilicon", servicehisiliconMethods);
-}
-#endif
