0b2597635493b2820f8b2d0d56fd7abeef2b31c9
jbleyel <jbleyel@gmx.net>
2025-03-04 16:31:45 +0100

    code format

--- servicehisilicon/pythonmodule.cpp
+++ servicehisilicon/pythonmodule.cpp
@@ -2,19 +2,19 @@
 
 static PyMethodDef servicehisiliconMethods[] =
 {
-	{NULL,NULL,0,NULL}
+	{NULL, NULL, 0, NULL}
 };
 
 static struct PyModuleDef moduledef = {
 	PyModuleDef_HEAD_INIT,
-	"servicehisilicon",		/* m_name */
-	"Module for servicehisilicon",	/* m_doc */
-	-1,				/* m_size */
-	servicehisiliconMethods,	/* m_methods */
-	NULL,				/* m_reload */
-	NULL,				/* m_traverse */
-	NULL,				/* m_clear */
-	NULL,				/* m_free */
+	"servicehisilicon",			   /* m_name */
+	"Module for servicehisilicon", /* m_doc */
+	-1,							   /* m_size */
+	servicehisiliconMethods,	   /* m_methods */
+	NULL,						   /* m_reload */
+	NULL,						   /* m_traverse */
+	NULL,						   /* m_clear */
+	NULL,						   /* m_free */
 };
 
 PyMODINIT_FUNC PyInit_servicehisilicon(void)
