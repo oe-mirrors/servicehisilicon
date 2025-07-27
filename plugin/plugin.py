e59adb177bdee0af83c28cb2468aafa4499b3963
jbleyel <jbleyel@gmx.net>
2025-07-27 18:39:19 +0200

    add new config.misc.disableServicehisilicon setting

--- plugin/plugin.py
+++ plugin/plugin.py
@@ -1,10 +1,18 @@
 from __future__ import absolute_import
 from Plugins.Plugin import PluginDescriptor
+from Components.config import config
+
 
 def autostart(reason, **kwargs):
 	from . import servicehisilicon
 
+
 def Plugins(**kwargs):
+	try:
+		if config.misc.disableServicehisilicon.value:
+			return []
+	except Exception:
+		pass
 	return [
-		PluginDescriptor(where = PluginDescriptor.WHERE_AUTOSTART, needsRestart = True, fnc = autostart)
+		PluginDescriptor(where=PluginDescriptor.WHERE_AUTOSTART, needsRestart=True, fnc=autostart)
 	]
